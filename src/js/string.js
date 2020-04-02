33002e90ac0dc5a2c8355a0a50b37f2cbe3165f9
Gabriel <gabriel.developer@outlook.com>
2020-04-01 23:04:49 -0300

    aprendendo sobre padding string

---
+++ src/js/string.js
@@ -0,0 +1,24 @@
+/**
+ * Padding start
+ */
+// const nome = 'Gabriel';
+// const sobrenome = 'Pereira';
+
+// const nomeCompleto = sobrenome.padStart(14, nome);
+
+// document.getElementById('stringPad').innerHTML = nomeCompleto;
+
+
+// const nomeCompletoPadEnd = nome.padEnd(14, sobrenome);
+
+// console.log(nomeCompletoPadEnd);
+
+const numeroCartao = '4890';
+
+const cartaoPreenchido = numeroCartao.padStart(16, '*');
+
+console.log(cartaoPreenchido);
+
+
+
+
